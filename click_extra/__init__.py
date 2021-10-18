a1b9291e8e975d5ba683f7023f33c933ae5993fb
Kevin Deldycke <kevin@deldycke.com>
2021-10-18 16:56:57 +0400

    Post release version bump.

--- click_extra/__init__.py
+++ click_extra/__init__.py
@@ -17,7 +17,7 @@
 
 """ Expose package-wide elements. """
 
-__version__ = "0.0.1"
+__version__ = "1.0.0"
 """ Examples of valid version strings according :pep:`440#version-scheme`:
 
 .. code-block:: python
