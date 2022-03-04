c483abaa8c8df528d16bd355adfc1ec9bcb6a057
kdeldycke <kdeldycke@users.noreply.github.com>
2022-03-04 12:02:36 +0000

    [changelog] Post-release version bump

--- click_extra/__init__.py
+++ click_extra/__init__.py
@@ -17,7 +17,7 @@
 
 """ Expose package-wide elements. """
 
-__version__ = "1.6.4"
+__version__ = "1.6.5"
 """ Examples of valid version strings according :pep:`440#version-scheme`:
 
 .. code-block:: python
