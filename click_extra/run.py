e2c5dd884c8f96076a582746322c91699894acfc
Kevin Deldycke <kevin@deldycke.com>
2022-03-31 18:08:10 +0400

    Remove click_extra.run.run and rebase all run utilities around subprocess.run.

--- click_extra/run.py
+++ click_extra/run.py
@@ -18,7 +18,7 @@
 """Utilities to execute external commands."""
 
 import os
-from subprocess import PIPE, Popen
+import subprocess
 from textwrap import indent
 
 from .colorize import theme
@@ -26,30 +26,6 @@ from .colorize import theme
 # Some CLI printing constants.
 PROMPT = "► "
 INDENT = " " * len(PROMPT)
-
-
-def run(*args, extra_env=None):
-    """Run a shell command, return error code, output and error message."""
-    assert isinstance(args, tuple)
-    assert extra_env is None or isinstance(extra_env, dict)
-
-    # Extend default environment with our extra variables, to mimick Python's original implementation. See;
-    # https://github.com/python/cpython/blob/7b5b429adab4fe0fe81858fe3831f06adc2e2141/Lib/subprocess.py#L1648-L1649
-    env = None
-    if extra_env:
-        env = os.environ
-        env.update(extra_env)
-
-    try:
-        process = Popen(args, stdout=PIPE, stderr=PIPE, env=env)
-    except OSError:
-        return None, None, f"`{args[0]}` executable not found."
-    output, error = process.communicate()
-    return (
-        process.returncode,
-        output.decode("utf-8") if output else None,
-        error.decode("utf-8") if error else None,
-    )
 
 
 def print_cli_output(cmd, output=None, error=None, error_code=None, extra_env=None):
@@ -72,8 +48,26 @@ def print_cli_output(cmd, output=None, error=None, error_code=None, extra_env=No
         print(theme.error(f"{INDENT}Return code: {error_code}"))
 
 
-def run_cmd(*args, extra_env=None):
+def extend_env(extra_env=None):
+    """Utility method to extend current environment variable.
+
+    Mimicks Python's original implementation. See:
+    https://github.com/python/cpython/blob/7b5b429adab4fe0fe81858fe3831f06adc2e2141/Lib/subprocess.py#L1648-L1649
+    """
+    assert not extra_env or isinstance(extra_env, dict)
+    env = None
+    if extra_env:
+        env = os.environ
+        env.update(extra_env)
+    return env
+
+
+def run_cmd(*args, extra_env=None, print_output=True):
     """Run a system command, print output and return results."""
-    code, output, error = run(*args, extra_env=extra_env)
-    print_cli_output(args, output, error, code, extra_env=extra_env)
-    return code, output, error
+    assert isinstance(args, tuple)
+    process = subprocess.run(args, capture_output=True, encoding="utf-8", env=extend_env(extra_env))
+
+    if print_output:
+        print_cli_output(args, process.stdout, process.stderr, process.returncode, extra_env=extra_env)
+
+    return process.returncode, process.stdout, process.stderr
