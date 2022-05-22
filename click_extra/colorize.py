7aebcf4f403b33fd69ce4918c31f5b37de712523
kdeldycke <kdeldycke@users.noreply.github.com>
2022-05-22 13:33:02 +0000

    [autofix] Format Python

--- click_extra/colorize.py
+++ click_extra/colorize.py
@@ -181,8 +181,8 @@ def color_option(
 
 
 class VersionOption(Option):
-    """No-op class wrapping ``Option`` to serve as a marker to identify
-    parameters created with our own ``version_option`` below."""
+    """No-op class wrapping ``Option`` to serve as a marker to identify parameters
+    created with our own ``version_option`` below."""
 
     pass
 
