6ab7e3b0d38b911a8fb094f08234a3c45f6c8450
kdeldycke <kdeldycke@users.noreply.github.com>
2022-09-30 07:04:54 +0000

    [changelog] Post-release version bump

--- click_extra/__init__.py
+++ click_extra/__init__.py
@@ -19,7 +19,7 @@
 
 from __future__ import annotations
 
-__version__ = "3.2.5"
+__version__ = "3.2.6"
 """ Examples of valid version strings according :pep:`440#version-scheme`:
 
 .. code-block:: python
