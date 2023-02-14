22f2c4687a081c10ca3044f64783ac83a88097d2
Kevin Deldycke <kevin@deldycke.com>
2023-02-14 19:24:01 +0400

    Fix linting.

--- click_extra/tests/test_colorize.py
+++ click_extra/tests/test_colorize.py
@@ -48,7 +48,7 @@ def test_theme_definition():
     logging primitives."""
     assert set(HelpTheme._fields).issubset(HelpExtraTheme._fields)
 
-    log_levels = {l.lower() for l in LOG_LEVELS}
+    log_levels = {level.lower() for level in LOG_LEVELS}
     assert log_levels.issubset(HelpExtraTheme._fields)
     assert log_levels.isdisjoint(HelpTheme._fields)
 
@@ -160,14 +160,20 @@ def test_keyword_collection(invoke):
         r"  \x1b\[36m--o4\x1b\[0m \x1b\[90mTEXT\x1b\[0m\n\n"
         r"\x1b\[94m\x1b\[1mOther options:\x1b\[0m\n"
         r"  \x1b\[36m--test\x1b\[0m \x1b\[90mTEXT\x1b\[0m\n"
-        r"  \x1b\[36m-b\x1b\[0m, \x1b\[36m--boolean\x1b\[0m / \x1b\[36m\+B\x1b\[0m, \x1b\[36m--no-boolean\x1b\[0m\n"
-        r"                            \x1b\[90m\[default: \x1b\[0m\x1b\[35mno-boolean\x1b\[0m\x1b\[90m\]\x1b\[0m\n"
-        r"  \x1b\[36m/debug\x1b\[0m; \x1b\[36m/no-debug\x1b\[0m         \x1b\[90m\[default: \x1b\[0m\x1b\[35mno-debug\x1b\[0m\x1b\[90m\]\x1b\[0m\n"
-        r"  \x1b\[36m--shout\x1b\[0m / \x1b\[36m-S\x1b\[0m, \x1b\[36m--no-shout\x1b\[0m  \x1b\[90m\[default: \x1b\[0m\x1b\[35mno-shout\x1b\[0m\x1b\[90m\]\x1b\[0m\n"
+        r"  \x1b\[36m-b\x1b\[0m, \x1b\[36m--boolean\x1b\[0m / \x1b\[36m\+B\x1b\[0m,"
+        r" \x1b\[36m--no-boolean\x1b\[0m\n"
+        r"                            "
+        r"\x1b\[90m\[default: \x1b\[0m\x1b\[35mno-boolean\x1b\[0m\x1b\[90m\]\x1b\[0m\n"
+        r"  \x1b\[36m/debug\x1b\[0m; \x1b\[36m/no-debug\x1b\[0m"
+        r"         \x1b\[90m\[default:"
+        r" \x1b\[0m\x1b\[35mno-debug\x1b\[0m\x1b\[90m\]\x1b\[0m\n"
+        r"  \x1b\[36m--shout\x1b\[0m / \x1b\[36m-S\x1b\[0m, \x1b\[36m--no-shout\x1b\[0m"
+        r"  \x1b\[90m\[default: \x1b\[0m\x1b\[35mno-shout\x1b\[0m\x1b\[90m\]\x1b\[0m\n"
         rf"{default_options_colored_help}"
         r"\n"
         r"\x1b\[94m\x1b\[1mSubcommand group 1:\x1b\[0m\n"
-        r"  \x1b\[36mcommand1\x1b\[0m  CLI description with extra \x1b\[90mMY_VAR\x1b\[0m reference.\n"
+        r"  \x1b\[36mcommand1\x1b\[0m  CLI description with extra"
+        r" \x1b\[90mMY_VAR\x1b\[0m reference.\n"
         r"  \x1b\[36mcommand2\x1b\[0m\n\n"
         r"\x1b\[94m\x1b\[1mExtra commands:\x1b\[0m\n"
         r"  \x1b\[36mcommand3\x1b\[0m\n"
@@ -187,37 +193,35 @@ def test_keyword_collection(invoke):
     # CLI main group is invoked before sub-command.
     result = invoke(color_cli1, "command1", "--help", color=True)
     assert result.exit_code == 0
-    assert result.output == dedent(
-        """\
-        It works!
-        \x1b[94m\x1b[1mUsage: \x1b[0m\x1b[97mcolor-cli1 command1\x1b[0m \x1b[90m[OPTIONS]\x1b[0m [\x1b[36mMY_ARG\x1b[0m]...
-
-          CLI description with extra MY_VAR reference.
-
-        \x1b[94m\x1b[1mPositional arguments:\x1b[0m
-          [\x1b[36mMY_ARG\x1b[0m]...  Argument supports help.
-
-        \x1b[94m\x1b[1mOptions:\x1b[0m
-          \x1b[36m-h\x1b[0m, \x1b[36m--help\x1b[0m  Show this message and exit.
-        """
+    assert result.output == (
+        "It works!\n"
+        "\x1b[94m\x1b[1mUsage: \x1b[0m\x1b[97mcolor-cli1 command1\x1b[0m"
+        " \x1b[90m[OPTIONS]\x1b[0m [\x1b[36mMY_ARG\x1b[0m]...\n"
+        "\n"
+        "  CLI description with extra MY_VAR reference.\n"
+        "\n"
+        "\x1b[94m\x1b[1mPositional arguments:\x1b[0m\n"
+        "  [\x1b[36mMY_ARG\x1b[0m]...  Argument supports help.\n"
+        "\n"
+        "\x1b[94m\x1b[1mOptions:\x1b[0m\n"
+        "  \x1b[36m-h\x1b[0m, \x1b[36m--help\x1b[0m  Show this message and exit.\n"
     )
     assert not result.stderr
 
     # Standalone call to command: CLI main group is skipped.
     result = invoke(command1, "--help", color=True)
     assert result.exit_code == 0
-    assert result.output == dedent(
-        """\
-        \x1b[94m\x1b[1mUsage: \x1b[0m\x1b[97mcommand1\x1b[0m \x1b[90m[OPTIONS]\x1b[0m [\x1b[36mMY_ARG\x1b[0m]...
-
-          CLI description with extra MY_VAR reference.
-
-        \x1b[94m\x1b[1mPositional arguments:\x1b[0m
-          [\x1b[36mMY_ARG\x1b[0m]...  Argument supports help.
-
-        \x1b[94m\x1b[1mOptions:\x1b[0m
-          \x1b[36m-h\x1b[0m, \x1b[36m--help\x1b[0m  Show this message and exit.
-        """
+    assert result.output == (
+        "\x1b[94m\x1b[1mUsage: \x1b[0m\x1b[97mcommand1\x1b[0m"
+        " \x1b[90m[OPTIONS]\x1b[0m [\x1b[36mMY_ARG\x1b[0m]...\n"
+        "\n"
+        "  CLI description with extra MY_VAR reference.\n"
+        "\n"
+        "\x1b[94m\x1b[1mPositional arguments:\x1b[0m\n"
+        "  [\x1b[36mMY_ARG\x1b[0m]...  Argument supports help.\n"
+        "\n"
+        "\x1b[94m\x1b[1mOptions:\x1b[0m\n"
+        "  \x1b[36m-h\x1b[0m, \x1b[36m--help\x1b[0m  Show this message and exit.\n"
     )
     assert not result.stderr
 
