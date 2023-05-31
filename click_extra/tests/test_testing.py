6e9c24e787e5e4c5d7f9c3314bf932b299cab1d7
Kevin Deldycke <kevin@deldycke.com>
2023-05-31 11:35:51 +0400

    Update docstring

--- click_extra/tests/test_testing.py
+++ click_extra/tests/test_testing.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-"""Test the testing utilities and CLI execution simulation."""
+"""Test the testing utilities and the simulation of CLI execution."""
 
 from __future__ import annotations
 
