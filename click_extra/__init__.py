92a5f029fd2f47d5f23d73085a505b05dffd267c
Kevin Deldycke <kevin@deldycke.com>
2024-04-04 21:17:14 +0400

    Fix lint

--- click_extra/__init__.py
+++ click_extra/__init__.py
@@ -101,121 +101,121 @@ from .testing import ExtraCliRunner  # noqa: E402
 from .timer import TimerOption  # noqa: E402
 from .version import ExtraVersionOption  # noqa: E402
 
-__all__ = [  # noqa: F405
-    "Abort",
-    "Argument",
-    "argument",
-    "BadArgumentUsage",
-    "BadOptionUsage",
-    "BadParameter",
-    "BaseCommand",
-    "BOOL",
-    "Choice",
-    "clear",
-    "ClickException",
-    "Color",
-    "color_option",
-    "ColorOption",
-    "Command",
-    "command",
-    "CommandCollection",
-    "config_option",
-    "ConfigOption",
-    "confirm",
-    "confirmation_option",
-    "constrained_params",
-    "constraint",
-    "ConstraintMixin",
-    "Context",
-    "DateTime",
-    "dir_path",
-    "echo",
-    "echo_via_pager",
-    "edit",
-    "extra_basic_config",
-    "extra_command",
-    "extra_group",
-    "extra_version_option",
-    "ExtraCliRunner",
-    "ExtraCommand",
-    "ExtraContext",
-    "ExtraGroup",
-    "ExtraLogFormatter",
-    "ExtraLogHandler",
-    "ExtraOption",
-    "ExtraVersionOption",
-    "File",
-    "file_path",
-    "FileError",
-    "FLOAT",
-    "FloatRange",
-    "format_filename",
-    "get_app_dir",
-    "get_binary_stream",
-    "get_current_context",
-    "get_text_stream",
-    "getchar",
-    "Group",
-    "group",
-    "help_option",
-    "HelpExtraFormatter",
-    "HelpExtraTheme",
-    "HelpFormatter",
-    "HelpOption",
-    "HelpSection",
-    "HelpTheme",
-    "INT",
-    "IntRange",
-    "launch",
-    "make_pass_decorator",
-    "MissingParameter",
-    "MultiCommand",
-    "NoSuchOption",
-    "open_file",
-    "Option",
-    "option",
-    "option_group",
-    "OptionGroup",
-    "OptionGroupMixin",
-    "OptionParser",
-    "Parameter",
-    "ParameterSource",
-    "ParamStructure",
-    "ParamType",
-    "pass_context",
-    "pass_obj",
-    "password_option",
-    "Path",
-    "path",
-    "pause",
-    "progressbar",
-    "prompt",
-    "search_params",
-    "secho",
-    "Section",
-    "SectionMixin",
-    "show_params_option",
-    "ShowParamsOption",
-    "STRING",
-    "Style",
-    "style",
-    "table_format_option",
-    "TableFormatOption",
-    "telemetry_option",
-    "TelemetryOption",
-    "timer_option",
-    "TimerOption",
-    "Tuple",
-    "UNPROCESSED",
-    "unstyle",
-    "UsageError",
-    "UUID",
-    "verbosity_option",
-    "VerbosityOption",
-    "version_option",
-    "VersionOption",
-    "warnings",
-    "wrap_text",
+__all__ = [
+    "Abort",  # noqa: F405
+    "Argument",  # noqa: F405
+    "argument",  # noqa: F405
+    "BadArgumentUsage",  # noqa: F405
+    "BadOptionUsage",  # noqa: F405
+    "BadParameter",  # noqa: F405
+    "BaseCommand",  # noqa: F405
+    "BOOL",  # noqa: F405
+    "Choice",  # noqa: F405
+    "clear",  # noqa: F405
+    "ClickException",  # noqa: F405
+    "Color",  # noqa: F405
+    "color_option",  # noqa: F405
+    "ColorOption",  # noqa: F405
+    "Command",  # noqa: F405
+    "command",  # noqa: F405
+    "CommandCollection",  # noqa: F405
+    "config_option",  # noqa: F405
+    "ConfigOption",  # noqa: F405
+    "confirm",  # noqa: F405
+    "confirmation_option",  # noqa: F405
+    "constrained_params",  # noqa: F405
+    "constraint",  # noqa: F405
+    "ConstraintMixin",  # noqa: F405
+    "Context",  # noqa: F405
+    "DateTime",  # noqa: F405
+    "dir_path",  # noqa: F405
+    "echo",  # noqa: F405
+    "echo_via_pager",  # noqa: F405
+    "edit",  # noqa: F405
+    "extra_basic_config",  # noqa: F405
+    "extra_command",  # noqa: F405
+    "extra_group",  # noqa: F405
+    "extra_version_option",  # noqa: F405
+    "ExtraCliRunner",  # noqa: F405
+    "ExtraCommand",  # noqa: F405
+    "ExtraContext",  # noqa: F405
+    "ExtraGroup",  # noqa: F405
+    "ExtraLogFormatter",  # noqa: F405
+    "ExtraLogHandler",  # noqa: F405
+    "ExtraOption",  # noqa: F405
+    "ExtraVersionOption",  # noqa: F405
+    "File",  # noqa: F405
+    "file_path",  # noqa: F405
+    "FileError",  # noqa: F405
+    "FLOAT",  # noqa: F405
+    "FloatRange",  # noqa: F405
+    "format_filename",  # noqa: F405
+    "get_app_dir",  # noqa: F405
+    "get_binary_stream",  # noqa: F405
+    "get_current_context",  # noqa: F405
+    "get_text_stream",  # noqa: F405
+    "getchar",  # noqa: F405
+    "Group",  # noqa: F405
+    "group",  # noqa: F405
+    "help_option",  # noqa: F405
+    "HelpExtraFormatter",  # noqa: F405
+    "HelpExtraTheme",  # noqa: F405
+    "HelpFormatter",  # noqa: F405
+    "HelpOption",  # noqa: F405
+    "HelpSection",  # noqa: F405
+    "HelpTheme",  # noqa: F405
+    "INT",  # noqa: F405
+    "IntRange",  # noqa: F405
+    "launch",  # noqa: F405
+    "make_pass_decorator",  # noqa: F405
+    "MissingParameter",  # noqa: F405
+    "MultiCommand",  # noqa: F405
+    "NoSuchOption",  # noqa: F405
+    "open_file",  # noqa: F405
+    "Option",  # noqa: F405
+    "option",  # noqa: F405
+    "option_group",  # noqa: F405
+    "OptionGroup",  # noqa: F405
+    "OptionGroupMixin",  # noqa: F405
+    "OptionParser",  # noqa: F405
+    "Parameter",  # noqa: F405
+    "ParameterSource",  # noqa: F405
+    "ParamStructure",  # noqa: F405
+    "ParamType",  # noqa: F405
+    "pass_context",  # noqa: F405
+    "pass_obj",  # noqa: F405
+    "password_option",  # noqa: F405
+    "Path",  # noqa: F405
+    "path",  # noqa: F405
+    "pause",  # noqa: F405
+    "progressbar",  # noqa: F405
+    "prompt",  # noqa: F405
+    "search_params",  # noqa: F405
+    "secho",  # noqa: F405
+    "Section",  # noqa: F405
+    "SectionMixin",  # noqa: F405
+    "show_params_option",  # noqa: F405
+    "ShowParamsOption",  # noqa: F405
+    "STRING",  # noqa: F405
+    "Style",  # noqa: F405
+    "style",  # noqa: F405
+    "table_format_option",  # noqa: F405
+    "TableFormatOption",  # noqa: F405
+    "telemetry_option",  # noqa: F405
+    "TelemetryOption",  # noqa: F405
+    "timer_option",  # noqa: F405
+    "TimerOption",  # noqa: F405
+    "Tuple",  # noqa: F405
+    "UNPROCESSED",  # noqa: F405
+    "unstyle",  # noqa: F405
+    "UsageError",  # noqa: F405
+    "UUID",  # noqa: F405
+    "verbosity_option",  # noqa: F405
+    "VerbosityOption",  # noqa: F405
+    "version_option",  # noqa: F405
+    "VersionOption",  # noqa: F405
+    "warnings",  # noqa: F405
+    "wrap_text",  # noqa: F405
 ]
 """Expose all of Click, Cloup and Click Extra.
 
