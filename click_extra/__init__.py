b21f43195370bdecb7fc1aa4391f47bf96bc2284
kdeldycke <159718+kdeldycke@users.noreply.github.com>
2025-11-05 18:56:36 +0000

    [changelog] Bump major version to v7.0.0

--- click_extra/__init__.py
+++ click_extra/__init__.py
@@ -225,7 +225,7 @@ __all__ = [  # noqa: F405
 """
 
 
-__version__ = "6.2.1"
+__version__ = "7.0.0"
 """Examples of valid version strings according :pep:`440#version-scheme`:
 
 .. code-block:: python
