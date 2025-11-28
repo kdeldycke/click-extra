f3f260ea94486a764f7057c9fcd49061cdfdb2ea
kdeldycke <159718+kdeldycke@users.noreply.github.com>
2025-11-28 13:18:35 +0000

    [changelog] Bump minor version to v7.3.0

--- click_extra/__init__.py
+++ click_extra/__init__.py
@@ -226,7 +226,7 @@ __all__ = [  # noqa: F405
 """
 
 
-__version__ = "7.2.1"
+__version__ = "7.3.0"
 """Examples of valid version strings according :pep:`440#version-scheme`:
 
 .. code-block:: python
