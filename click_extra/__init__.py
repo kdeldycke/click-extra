6bfd4270927e1655889d572748b395fe4e0c49e2
Kevin Deldycke <kevin@deldycke.com>
2025-11-28 17:23:02 +0400

    No need for version example

--- click_extra/__init__.py
+++ click_extra/__init__.py
@@ -227,17 +227,6 @@ __all__ = [  # noqa: F405
 
 
 __version__ = "7.3.0"
-"""Examples of valid version strings according :pep:`440#version-scheme`:
-
-.. code-block:: python
-
-    __version__ = "1.2.3.dev1"  # Development release 1
-    __version__ = "1.2.3a1"  # Alpha Release 1
-    __version__ = "1.2.3b1"  # Beta Release 1
-    __version__ = "1.2.3rc1"  # RC Release 1
-    __version__ = "1.2.3"  # Final Release
-    __version__ = "1.2.3.post1"  # Post Release 1
-"""
 
 
 def __getattr__(name: str) -> object:
