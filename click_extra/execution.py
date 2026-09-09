9f92e445af6f267ffa95ccb18a64b5b50a30bfb1
Kevin Deldycke <kevin@deldycke.com>
2026-09-09 16:24:45 +0400

    Let the highlighter mark the `--jobs` keywords, instead of quotes

--- click_extra/execution.py
+++ click_extra/execution.py
@@ -343,8 +343,8 @@ class JobsOption(ExtraOption):
         show_default=True,
         type=JobCount(),
         help=_(
-            "Number of parallel jobs. Accepts an integer, 'auto' (the host's "
-            "logical CPUs minus one) or 'max' (all logical CPUs). 0 runs "
+            "Number of parallel jobs. Accepts an integer, auto (the host's "
+            "logical CPUs minus one) or max (all logical CPUs). 0 runs "
             "sequentially."
         ),
         **kwargs,
